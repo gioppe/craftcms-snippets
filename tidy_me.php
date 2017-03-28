88e21c3c47d9b809a8b317053925357fe46a71a8
gioppe <gioppe@gmail.com>
2017-03-28 16:39:52 +0200

    Update tidy_me.php
    Added two functions

--- tidy_me.php
+++ tidy_me.php
@@ -36,6 +36,45 @@ namespace Craft;
 		return $successo;
 	}
 
+	function nuovaEntry($arr_dati) // title required
+	{
+		$entry = new EntryModel();
+        	$entry->sectionId  = 1;
+        	$entry->typeId     = 1;
+        	$entry->enabled    = true;
+        	$entry->getContent()->setAttributes($arr_dati);
+        	$successo = craft()->entries->saveEntry($entry);
+
+		return $successo;
+	}
+
+	function trovaIdBlocco( $nome_blocco, $campo_id )
+	{
+		$tipi = craft()->matrix->getBlockTypesByFieldId($campo_id);
+		$blocktype_id = false;
+		foreach($tipi as $tipo)
+		{
+			if($tipo->handle == $nome_blocco) $blocktype_id = $tipo->id;
+		}
+		return $blocktype_id;
+	}
+
+	function eliminaBlocchi($entry_id, $nome_matrix, $locale)
+	{
+		// ELIMINA tutte le righe di un matrix
+
+		$entry = craft()->entries->getEntryById( $entry_id, $locale );
+		$matrix = $entry->$nome_matrix;
+
+		foreach($matrix as $blocco)
+		{
+			$blocchi_da_cancellare[] = $blocco->id;
+		}
+
+		$esito = craft()->matrix->deleteBlockById($blocchi_da_cancellare);
+		return $esito;
+	}
+
 
 // $row->enabled = false;
 
