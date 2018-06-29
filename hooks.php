6a7356434cb65c7969022b50cc96186d2d1be9e4
gioppe <gioppe@gmail.com>
2018-06-29 11:08:38 +0200

    restart from scratch for Craft3

--- hooks.php
+++
@@ -1,8 +0,0 @@
-// insert into main Plugin class 
-
-// overrides default routing behaviour
-public function getElementRoute(BaseElementModel $element)
-{
-  $percorso = craft()->templates->getTemplatesPath() . 'myPath/';
-  craft()->templates->setTemplatesPath($alternative_path);
-}
