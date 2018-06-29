4876055b5587d6a3c1a60a7d8ab91d8addd691f9
gioppe <gioppe@gmail.com>
2018-06-29 11:17:37 +0200

    new

---
+++ entries.php
@@ -0,0 +1,30 @@
+<?php
+
+// ———————————————————————————
+// Get Entries 
+
+$entry = Entry::find();
+$entry->section($mySection);
+$entry->site($siteHandle);
+// chain parameters...
+$entry->all();
+
+// ———————————————————————————
+// Modify and entry
+
+$entry = Entry::find()->id( $myId )->one();
+
+// Set title
+
+$entry->title = 'New title';
+
+// Set site
+$entry->siteId = $newSiteId;
+
+// Set other fields
+$fields = [
+  'fieldHandle' => $newFieldValue
+];
+$entry->setFieldValues( $fields );
+
+Craft::$app->getElements()->saveElement( $entry );
