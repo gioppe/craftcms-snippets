72ca525082f3bb2e7414dd0f5d5f64ebd70d1f6e
gioppe <gioppe@gmail.com>
2018-07-03 16:43:17 +0200

    some work

--- http.php
+++ http.php
@@ -4,4 +4,14 @@
 
 $post = Craft::$app->request->post();    
 
+// will accept a JSON response?
 
+$ifAcceptsJson = Craft::$app->getRequest()->getAcceptsJson()
+
+// do it!
+  
+return $this->asJson( $response );
+
+// no wait, send a redirect instead
+
+$this->redirect( $newRoute );
