48c78e9b321e03867e3061b655f7257f40175561
gioppe <gioppe@gmail.com>
2018-07-04 17:09:42 +0200

    Update commerce.php

--- commerce.php
+++ commerce.php
@@ -3,6 +3,12 @@
 // load Commerce interface, alias to something not generic as "Plugin"
 use craft\commerce\Plugin as CraftCommerce;
 
+// get current Cart
+
+CraftCommerce::getInstance()->getCarts()->getCart();
+
+// get country list
+
 CraftCommerce::getInstance()->getCountries()->getAllCountries()
 
 // Search variants
