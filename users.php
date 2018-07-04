f6bf49d0a03288d79263397414a05c5b339c4449
gioppe <gioppe@gmail.com>
2018-07-04 11:39:25 +0200

    Update users.php

--- users.php
+++ users.php
@@ -7,3 +7,20 @@ Craft::$app->user->getIsAdmin()
   
 // get a user
 Craft::$app->users->getUserByUsernameOrEmail( $usernameOrEmail );
+
+// create a new user
+use craft\elements\User;
+
+$user = new User;
+$user->email     = $myEmail;
+$user->username  = $myUsernameorEmail;
+$user->firstName = $myFirstName;
+$user->lastName  = $myLastName;
+// $user->pending = true;
+
+$user->setFieldValues([		
+  'myField' => 'myValue',
+]);
+
+$user->validate();
+$success = Craft::$app->getElements()->saveElement($user, false);
