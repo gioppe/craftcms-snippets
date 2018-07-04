bb75f14d65fc30c8afe0ea147aef8983b32238d8
gioppe <gioppe@gmail.com>
2018-07-04 11:15:58 +0200

    Update users.php

--- users.php
+++ users.php
@@ -2,3 +2,8 @@
 
 // check if current logged user is Admin
 Craft::$app->user->getIsAdmin()
+
+// Refer: https://docs.craftcms.com/api/v3/craft-services-users.html  
+  
+// get a user
+Craft::$app->users->getUserByUsernameOrEmail( $usernameOrEmail );
