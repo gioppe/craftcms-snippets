165a2664deec38bd4e08a338d6db796723fda068
gioppe <gioppe@gmail.com>
2018-07-20 12:36:22 +0200

    Update matrix.php

--- matrix.php
+++ matrix.php
@@ -1,5 +1,28 @@
 <?php
+use craft\elements\MatrixBlock;
 
 // delete a block
 
 Craft::$app->elements->deleteElement($block);
+
+
+// new block
+
+$matrixField = Craft::$app->fields->getFieldByHandle('matrixFieldHandle');
+$blockTypes  = Craft::$app->getMatrix()->getBlockTypesByFieldId( $matrixField->id );
+
+$block = new MatrixBlock;
+$block->fieldId = $matrixField->id;
+$block->ownerId = $entry->id; 
+$block->typeId  = $blockTypes[ $blockIndex ]->id;  // Fix this...
+$block->ownerSiteId = $ownerSiteId;
+$block->siteId = $siteId;
+
+		$block->setFieldValues([
+			'nome'      => 'ciaoFR2',
+			'valore'    => 'fooFR',
+			'unita'     => 'barFR',
+			'normativa' => 'quezFR'
+		]);
+
+$success = Craft::$app->getElements()->saveElement( $block, true, false );
