836a3089c670e65341924688808eb9c0f5a3d66a
gioppe <gioppe@gmail.com>
2018-09-26 10:34:52 +0200

    Create controllers.php

---
+++ controllers.php
@@ -0,0 +1,25 @@
+<?php
+
+// boilerplate...
+namespace modules\mymodule\controllers;
+
+use modules\mymodule\MyModule;
+
+use Craft;
+use craft\web\Controller;
+
+class FooController extends Controller
+{
+
+  protected $allowAnonymous = ['bar', 'baz-quez'];
+  
+  public function actionBar() {
+    // responds to: actions/my-module/foo/bar      
+  }
+  
+  public function actionBazQuez() {
+    // responds to: actions/my-module/foo/bar-quez
+  }
+  
+
+}
