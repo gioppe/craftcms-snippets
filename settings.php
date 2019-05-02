22742c7817b34e83b866ee91abc332de531748a8
gioppe <gioppe@gmail.com>
2019-05-02 09:36:37 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,10 +1,15 @@
 <?php
 
 // get current Site:
-
 $site = Craft::$app->sites->getCurrentSite()->handle
+  
+// get a site
+$site = Craft::$app->sites->getSiteByHandle($siteHandle);
 
+// get environment
+$env = Craft::$app->config->env // "dev"
   
 // is devMode enabled?
-  
 $ifDevMode = Craft::$app->config->getGeneral()->devMode,
+
+
