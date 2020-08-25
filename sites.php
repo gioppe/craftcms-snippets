f580d518e8f5e6d608804e6d0d5942c500376ac6
gioppe <gioppe@gmail.com>
2020-08-25 08:53:45 +0200

    Update sites.php

--- sites.php
+++ sites.php
@@ -6,3 +6,5 @@ $site = Craft::$app->sites->getCurrentSite()->handle
 // get a site
 $site = Craft::$app->sites->getSiteByHandle($siteHandle);
 
+// get a localized entry
+{% set localizedEntry = craft.entries.id(entry.id).site('otherSiteHandle').one() %}
