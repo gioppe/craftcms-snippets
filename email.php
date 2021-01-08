07107268cd265fa869ee06a46113bf6f766549a1
gioppe <gioppe@gmail.com>
2021-01-08 11:01:12 +0100

    Update email.php

--- email.php
+++ email.php
@@ -5,6 +5,6 @@ use craft\mail\Message;
 $msg = new Message();
 $msg->setTo('recipient@email.com');
 $msg->setSubject("New email subject");
-$template = Craft::$app->getView()->renderTemplate('path/to/template');
-$msg->setHtmlBody($template, ['data' => 'someData']));
+$template = Craft::$app->getView()->renderTemplate('path/to/template', ['data' => 'someData']);
+$msg->setHtmlBody($template);
 $msg->send();
