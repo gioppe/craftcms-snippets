81216b187c7ff1467f32b4e1a72da700c20ffda3
gioppe <gioppe@gmail.com>
2021-01-08 10:18:45 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -5,4 +5,5 @@ $env = Craft::$app->config->env // "dev"
 // is devMode enabled?
 $ifDevMode = Craft::$app->config->getGeneral()->devMode,
 
-
+// get plugin setting
+$var = PluginHandle::getInstance()->settings->varName;
