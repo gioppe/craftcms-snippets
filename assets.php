bcc712c9019dca7c9a98880d673f87a533b4450f
gioppe <gioppe@gmail.com>
2021-01-22 09:24:12 +0100

    Create assets.php

---
+++ assets.php
@@ -0,0 +1,18 @@
+<?php
+use craft\elements\Asset as AssetElement;
+
+// save a new image
+
+$asset = new AssetElement();
+$asset->tempFilePath = $tempPath; // physical path to new image
+$asset->filename = $filename;
+$asset->newFolderId = $folderId;
+$asset->volumeId = $volumeId;
+$asset->avoidFilenameConflicts = true;
+$asset->setScenario(AssetElement::SCENARIO_CREATE);
+
+$out = Craft::$app->getElements()->saveElement($asset);
+
+// associate to entry
+$field = Craft::$app->fields->getFieldByHandle('imageFieldHandle');
+Craft::$app->relations->saveRelations($field , $entry, $arrayOfAssetIds );
